72512f2f84e54b0ebefd036480b64e869e39e290
MOAH <357668900@qq.com>
2017-02-28 22:55:48 +0800

    daydayup

---
+++ ch01/1.py
@@ -0,0 +1,14 @@
+# -- coding: utf-8 --
+print 10<20
+t=(1,'abc',0.4)
+print t
+print 0.4 in t
+
+d={1:'1','abc':0.1,0.4:80}
+print 'abc'in d
+
+#只能考虑是否有键值key
+print 0.1 in d
+
+import math
+print math.exp(2)
